3d4ec04d5e677b7f00937fe8a8dd59d956aa27fb
Deniss Akisins <Denjaa3@icloud.com>
2023-04-29 21:15:23 +0300

    IBOutlets setup

--- RGBLights/ViewController.swift
+++ RGBLights/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewRGB: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redSliderLabel: UILabel!
+    @IBOutlet var greenSliderLabel: UILabel!
+    @IBOutlet var blueSliderLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewRGB.layer.cornerRadius = 10
+        redSliderLabel.text = redSlider.value.formatted()
     }
 
 
